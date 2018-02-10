846b2a4ceab8652ff2bbd8b4d6e389ce333ece78
Alejandro <silvestroalejandro@gmail.com>
2018-02-09 23:16:20 -0300

    Agregado consulta

--- src/com/company/Empleado.java
+++ src/com/company/Empleado.java
@@ -12,11 +12,35 @@ public class Empleado {
     private int ano;
     private Date Alta;
 
-    public Empleado (String nombre,double sueldo,int dia,int mes, int ano){
+    public Empleado (String nombre, double sueldo, int dia, int mes, int ano){
 
         this.nombre = nombre;
         this.sueldo = sueldo;
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
         GregorianCalendar fech_Actual = new GregorianCalendar(ano,mes-1,dia);
         Alta = fech_Actual.getTime();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Date getAlta() {
+        //String f_alta;
+        //f_alta = Alta.;
+
+        return Alta;
+    }
+
+    public void SubeSueldo (double porcentaje){
+
+        sueldo += (sueldo*porcentaje)/100;
+    }
+
+    public double getSueldo(){
+
+        return sueldo;
     }
 }
--- src/com/company/Uso_empleado.java
+++ src/com/company/Uso_empleado.java
@@ -4,6 +4,13 @@ public class Uso_empleado {
 
     public static void main (String[] args){
 
+        Empleado Maria = new Empleado("María", 1000,12,3,1990);
+
+        System.out.println("El empleado " + Maria.getNombre() + " ingreso el " + Maria.getAlta());
+
+        //Maria.SubeSueldo(10);
+
+        System.out.println("Su sueldo es: "+ Maria.getSueldo());
 
     }
 }

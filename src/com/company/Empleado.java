cd0d6f5ed6e89e04954a5f067220e54022c6c1ee
Alejandro <silvestroalejandro@gmail.com>
2018-02-09 21:42:44 -0300

    Clase empleado

---
+++ src/com/company/Empleado.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Empleado {
+
+    private String nombre;
+    private double sueldo;
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public Empleado (String nombre,double sueldo,int dia,int mes, int ano){
+
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+}
